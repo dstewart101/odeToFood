17b8f5a1f320fd0cce91a53a1680a82c9292c0c3
David Stewart <david.stewart@communities-ni.gov.uk>
2018-07-06 15:43:01 +0100

    validation on create model

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OdeToFood.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Restaurant Name")]
+        [MaxLength(15, ErrorMessage = "Name is too long")]
         public String Name { get; set; }
+
         public CuisineType Cuisine { get; set; }
     }
 }
--- OdeToFood/ViewModels/RestaurantEditViewModel.cs
+++ OdeToFood/ViewModels/RestaurantEditViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using OdeToFood.Models;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OdeToFood.ViewModels
 {
     public class RestaurantEditViewModel
     {
+        [Required]
+        [Display(Name = "Restaurant Name")]
+        [MaxLength(15, ErrorMessage = "Name is too long")]
         public String Name { get; set; }
         public CuisineType Cuisine { get; set; }
     }
