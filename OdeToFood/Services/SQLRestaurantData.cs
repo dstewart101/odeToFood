9975d818b349e9ce4d1c89b7d8bc8ca5a4a42f33
David Stewart <david.stewart@communities-ni.gov.uk>
2018-07-09 08:55:00 +0100

    EntityFramework implemented. Db swapped in for In Memory Implementation

---
+++ OdeToFood/Services/SQLRestaurantData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Data;
+using OdeToFood.Interfaces;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class SQLRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _context;
+
+        public SQLRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy();
+        }
+
+        public Restaurant GetSingle(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
